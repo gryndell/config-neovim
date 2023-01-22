ef3cc27ced39f33f187fa4c72b4191f9792b0953
gryndell <alfred.pengelly@gmail.com>
2023-01-22 14:32:51 +1000

    Changed javascript indent to 2 spaces in aucommands.lua

--- lua/aucommands.lua
+++ lua/aucommands.lua
@@ -52,6 +52,7 @@ augroup file_types
     \ setlocal wrap linebreak formatoptions=tcqn
   autocmd FileType markdown,nroff,pandoc,tex,text   setlocal formatprg=par\ -w79
   autocmd FileType c,cpp,javascript,lua,rust,sh,vim setlocal cindent
+  autocmd FileType javascript setlocal tabstop=2 shiftwidth=2
 
   " Fold Methods
   autocmd FileType markdown setlocal foldmethod=expr
